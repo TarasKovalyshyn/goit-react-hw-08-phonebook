e61a6df0d1aa5f5d767461f04157799625c31e22
TarasKovalyshyn <taras.kovalyshyn@gmail.com>
2023-01-31 13:27:21 +0200

    commit

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,6 +1,4 @@
-export const selectFilter = state => state.contacts.filter;
-
-// export const selectFilter = state => state.filter;
+export const selectFilter = state => state.filter;
 export const getContacts = state => state.contacts.items;
 export const getIsLoading = state => state.contacts.isLoading;
 export const getError = state => state.contacts.error;
