1caa3d36ba7b20ac192ec16829cf88daa3a81d17
TarasKovalyshyn <taras.kovalyshyn@gmail.com>
2023-02-07 21:16:58 +0000

    Deploying to gh-pages from @ TarasKovalyshyn/goit-react-hw-08-phonebook@bdcbe2bd6cedb943935c65a33cae75cb9748e0aa 🚀

--- static/js/266.756e37da.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[266],{1266:function(e,t,n){n.r(t),n.d(t,{default:function(){return o}});var i=n(184),c={container:{minHeight:"calc(100vh - 50px",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeigth:500,fontSize:48,textAlign:"center"}};function o(){return(0,i.jsx)("div",{style:c.container,children:(0,i.jsx)("h1",{style:c.title,children:" Contacts manager welcome page "})})}}}]);
-//# sourceMappingURL=266.756e37da.chunk.js.map
